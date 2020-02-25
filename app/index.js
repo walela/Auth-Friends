ab028a5bc9ae75fe2abeaf25ebd6506fe2051d1e
Austin Walela <austinmasibo@gmail.com>
2020-02-25 17:09:53 +0300

    Initial commit - project setup

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <h1>Hello, from React!</h1>;
+}
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<App />, root);
